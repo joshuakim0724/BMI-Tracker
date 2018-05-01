a155c2d49288d033516c5b6fde1f4d7d18761d71
joshuakim0724 <joshuakim0724@gmail.com>
2018-05-01 13:07:58 -0500

    Woah my main file reappeared this is really weird send help Rohin

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "ofMain.h"
+#include "ofApp.h"
+#include "user.h"
+#include <iostream>
+#include <sstream>
+
+int main( )
+{
+    ofSetupOpenGL(1920, 1080, OF_WINDOW);
+    ofRunApp(new ofApp());
+}
